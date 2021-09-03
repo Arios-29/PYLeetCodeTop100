1208ba7726c3c8087256a5df931cb0847909ebcc
ChenYu <2508335092@qq.com>
2021-09-03 17:53:27 +0800

    两数相加

--- com/solutions/SolutionTwo.py
+++ com/solutions/SolutionTwo.py
@@ -11,40 +11,39 @@ class SolutionTwo:
         tmp = 0
         while l1 is not None and l2 is not None:
             sum = l1.val + l2.val + tmp
-            if sum >=10:
-                sum=sum-10
-                tmp=1
+            if sum >= 10:
+                sum = sum - 10
+                tmp = 1
             else:
-                tmp=0
+                tmp = 0
             node = ListNode(sum)
-            cur.next=node
-            cur=cur.next
-            l1=l1.next
-            l2=l2.next
+            cur.next = node
+            cur = cur.next
+            l1 = l1.next
+            l2 = l2.next
         while l1 is not None:
             sum = l1.val + tmp
-            if sum >=10:
-                sum=sum-10
-                tmp=1
+            if sum >= 10:
+                sum = sum - 10
+                tmp = 1
             else:
-                tmp=0
+                tmp = 0
             node = ListNode(sum)
-            cur.next=node
-            cur=cur.next
-            l1=l1.next
+            cur.next = node
+            cur = cur.next
+            l1 = l1.next
         while l2 is not None:
-            sum = l2.val+tmp
-            if sum>=10:
-                sum=sum-10
-                tmp=1
+            sum = l2.val + tmp
+            if sum >= 10:
+                sum = sum - 10
+                tmp = 1
             else:
-                tmp=0
+                tmp = 0
             node = ListNode(sum)
-            cur.next=node
-            cur=cur.next
-            l2=l2.next
-        if tmp==1:
+            cur.next = node
+            cur = cur.next
+            l2 = l2.next
+        if tmp == 1:
             node = ListNode(1)
-            cur.next=node
+            cur.next = node
         return head.next
-
