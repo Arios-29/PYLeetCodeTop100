4c1ae258ea19d6a3fa1d40e138746d92639a081c
ChenYu <2508335092@qq.com>
2021-09-04 20:01:45 +0800

    寻找两个正序数组的中位数

---
+++ com/solutions/SolutionFour.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class SolutionFour:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+       nums=nums1+nums2
+       nums.sort()
+       amount=len(nums)
+       if amount%2==0:
+           return (nums[amount//2-1]+nums[amount//2])/2
+       else:
+           return float(nums[amount//2])
