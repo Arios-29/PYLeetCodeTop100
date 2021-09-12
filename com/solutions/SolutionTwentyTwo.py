6765922ac10379fe56455fd891b9b5eb87cd19bb
ChenYu <2508335092@qq.com>
2021-09-12 19:07:03 +0800

    括号生成

---
+++ com/solutions/SolutionTwentyTwo.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        if n == 0:
+            return [""]
+        ans = []
+        for i in range(n):
+            for left in self.generateParenthesis(i):
+                for right in self.generateParenthesis(n - 1 - i):
+                    ans.append("(" + left + ")" + right)
+        return ans
