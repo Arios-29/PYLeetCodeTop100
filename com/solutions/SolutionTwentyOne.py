e6ef430a38f1e62a6562165429fab4e7a2f7fda5
ChenYu <2508335092@qq.com>
2021-09-12 19:01:31 +0800

    合并有序链表

---
+++ com/solutions/SolutionTwentyOne.py
@@ -0,0 +1,23 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        head = ListNode()
+        pre = head
+        while l1 is not None and l2 is not None:
+            if l1.val <= l2.val:
+                pre.next = l1
+                l1 = l1.next
+            else:
+                pre.next = l2
+                l2 = l2.next
+            pre = pre.next
+        if l1 is None:
+            pre.next = l2
+        else:
+            pre.next = l1
+        return head.next
