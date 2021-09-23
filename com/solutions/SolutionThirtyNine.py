bd3d38115c421cb011ee9287959c577f34ed98fd
ChenYu <2508335092@qq.com>
2021-09-23 15:43:17 +0800

    组合总和

---
+++ com/solutions/SolutionThirtyNine.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class SolutionThirtyNine:
+    def dfs(self, candidates: List[int], target: int, ans: List[List[int]], index: int, state: List[int], sum: int):
+        if sum == target:
+            ans.append(state)
+        elif sum > target:
+            return None
+        else:
+            for i in range(index, len(candidates)):
+                nextState = state.copy()
+                nextState.append(candidates[i])
+                self.dfs(candidates, target, ans, i, nextState, sum + candidates[i])
+
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        ans = []
+        start = []
+        self.dfs(candidates, target, ans, 0, start, 0)
+        return ans
